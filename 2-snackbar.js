e29bb396ea6ac6fc26652327504824eec4d3381b
Ihor-Kotliarevskyi <jagor0707@gmail.com>
2025-10-06 18:40:50 +0000

    Deploying to gh-pages from @ Ihor-Kotliarevskyi/goit-js-hw-10@91e0960e8e5726aa7637fc4bd2b5dc5cad1b67d6 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const t=document.querySelector(".form");document.querySelector("button");t.addEventListener("submit",o);function o(e){console.log(e.element)}
+//# sourceMappingURL=2-snackbar.js.map
