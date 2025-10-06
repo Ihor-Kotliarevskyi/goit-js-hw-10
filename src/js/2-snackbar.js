873b32630c009f1dd7388dd87108a0c480642d52
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-10-06 22:53:23 +0300

    Added and set iziToast

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,16 +1,35 @@
-// import iziToast from 'izitoast';
-// import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
+const delay = document.querySelector('.form input');
 const btn = document.querySelector('button');
-
+const a = 1;
+const b = 2;
 form.addEventListener('submit', handlerSubmit);
 
 function handlerSubmit(event) {
-  console.log(event.element);
+  event.preventDefault();
+  const title = delay.value;
+  const message = event.target.elements.state.value;
+  if (event.target.elements.state.value === 'fulfilled') {
+    iziToast.success({
+      title: `OK`,
+      message: `Delay: ${title}`,
+      position: 'topCenter',
+      timeout: 5000,
+      progressBar: true,
+      transitionIn: 'fadeInDown',
+    });
+  } else {
+    iziToast.error({
+      title: `Error`,
+      message: `Delay: ${title}`,
+      position: 'topCenter',
+      timeout: 5000,
+      progressBar: true,
+      transitionIn: 'fadeInDown',
+    });
+  }
+  form.reset();
 }
-
-// iziToast.show({
-//   title: `${}`,
-//   message: `${}`,
-// });
