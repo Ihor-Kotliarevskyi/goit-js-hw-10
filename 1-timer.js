0cf600efb38dd4c90dc89d32743673a12a0855ae
Ihor-Kotliarevskyi <jagor0707@gmail.com>
2025-10-06 19:53:33 +0000

    Deploying to gh-pages from @ Ihor-Kotliarevskyi/goit-js-hw-10@873b32630c009f1dd7388dd87108a0c480642d52 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as t,i as o}from"./assets/vendor-DRBXA5vM.js";const i={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",i);o.show({title:"Hey",message:"What would you like to add?"});
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as t,i as o}from"./assets/vendor-fyLdAcWF.js";const i={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",i);o.show({title:"Hey",message:"What would you like to add?"});
 //# sourceMappingURL=1-timer.js.map
