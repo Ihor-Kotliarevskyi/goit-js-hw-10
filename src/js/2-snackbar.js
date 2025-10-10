36d379b8d1f2331f8d107384126c2b306c4c4660
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-10-10 23:14:21 +0300

    added promise to snackbar

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,28 +3,60 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 const delayInput = document.querySelector('.form input');
-form.addEventListener('submit', handlerSubmit);
 
-function handlerSubmit(event) {
+form.addEventListener('submit', createPromise);
+
+function createPromise(event) {
   event.preventDefault();
+
   const delay = delayInput.value;
-  const message = event.target.elements.state.value;
-  if (event.target.elements.state.value === 'fulfilled') {
-    iziToast.success({
-      message: `Fulfilled promise in ${delay}ms`,
-      position: 'topCenter',
-      timeout: 5000,
-      progressBar: false,
-      transitionIn: 'fadeInDown',
+  const state = event.target.elements.state.value;
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  })
+    .then(delay => {
+      iziToast.success({
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        progressBar: false,
+        transitionIn: 'fadeInDown',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+        progressBar: false,
+        transitionIn: 'fadeInDown',
+      });
     });
-  } else {
-    iziToast.error({
-      message: `Rejected promise in ${delay}ms`,
-      position: 'topCenter',
-      timeout: 5000,
-      progressBar: false,
-      transitionIn: 'fadeInDown',
-    });
-  }
+
+  // if (state === 'fulfilled') {
+  //   iziToast.success({
+  //     message: `Fulfilled promise in ${delay}ms`,
+  //     position: 'topRight',
+  //     timeout: 50000,
+  //     progressBar: false,
+  //     transitionIn: 'fadeInDown',
+  //   });
+  // } else {
+  //   iziToast.error({
+  //     message: `Rejected promise in ${delay}ms`,
+  //     position: 'topRight',
+  //     timeout: 50000,
+  //     progressBar: false,
+  //     transitionIn: 'fadeInDown',
+  //   });
+  // }
+
   form.reset();
 }
